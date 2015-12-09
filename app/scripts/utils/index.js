c18b130d681745d7b148bbba6a4505120e5a7a1c
Daniel Levitt <daniellevitt86@gmail.com>
2015-12-09 23:48:38 +0000

    Path configuration of important files

--- app/scripts/utils/index.js
+++ app/scripts/utils/index.js
@@ -2,5 +2,6 @@ export { default as createConstants } from './createConstants';
 export { default as createReducer } from './createReducer';
 export { default as flatten } from './flatten';
 export { default as konami } from './konami';
+export { default as pathConfig } from './pathConfig';
 export { default as swatches } from './swatches';
 export { default as toasterInstance } from './toaster';
---
+++ app/scripts/utils/pathConfig.js
@@ -0,0 +1,28 @@
+import fs from 'fs-extra';
+import path from 'path';
+import remote from 'remote';
+
+let exportData;
+
+if (__DESKTOP__) {
+  const app = remote.require('app');
+
+  // Setup directories
+  [ 'user-sounds', 'user-data' ].forEach(_d => fs.ensureDir(path.join(app.getPath('userData'), _d)));
+
+  exportData = {
+    // Default json objects & dirs
+    gradientFile: path.join(app.getAppPath(), 'data/gradients.json'),
+    settingsFile: path.join(app.getAppPath(), 'data/settings.json'),
+    soundFile: path.join(app.getAppPath(), 'data/sounds.json'),
+    soundDir: path.join(app.getAppPath(), 'sounds'),
+
+    // User data & dirs
+    userSoundDir: path.join(app.getPath('userData'), 'user-sounds'),
+    userSettingsFile: path.join(app.getPath('userData'), 'user-data/settings.json'),
+    userSoundFile: path.join(app.getPath('userData'), 'user-data/sounds.json'),
+    userInstallFile: path.join(app.getPath('userData'), 'user-data/app-details.json')
+  };
+}
+
+export default exportData;
