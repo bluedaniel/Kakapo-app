374f24c73ea9cfb94f2928792a0fd41d5b573475
Daniel Levitt <daniel.levitt@guardian.co.uk>
2015-12-20 01:21:47 +0000

    PostCSS task and app.html fixes

--- tools/build.js
+++ tools/build.js
@@ -3,5 +3,6 @@ import run from './run';
 export default async function build() {
   await run(require('./clean'));
   await run(require('./copy'));
+  await run(require('./css'));
   await run(require('./bundle'));
 }
