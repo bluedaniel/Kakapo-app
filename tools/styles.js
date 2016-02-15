568e5a9c060b41cc0646144816a4c597cf3a70e2
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-02-15 23:45:33 +0000

    Update packages and styles in build

--- tools/styles.js
+++ tools/styles.js
@@ -3,11 +3,14 @@ import postcss from 'postcss';
 import postcssPlugins, { postcssImport } from './postcss.plugins';
 import cssnano from 'cssnano';
 
+const cssDir = './app/css';
+
 export default async function styles() {
-  // Minify CSS files
-  const cssMin = [ 'downloads', 'loading' ].map(file =>
+
+  // Minify external CSS files
+  const cssMin = fs.readdirSync(`${cssDir}/external`).map(file =>
     new Promise((resolve, reject) => {
-      const source = fs.readFileSync(`./app/css/${file}.css`, 'utf8');
+      const source = fs.readFileSync(`${cssDir}/external/${file}`, 'utf8');
 
       postcss(postcssPlugins.concat(postcssImport())).process(source, {
         from: './app/scripts/styles'
@@ -26,24 +29,16 @@ export default async function styles() {
     })
   );
 
+  const targetHtml = './build/index.html';
+
   // Inline some smaller CSS files into the HTML directly
-  const inlineOpts = [ {
-    html: './build/index.html',
-    css: './build/css/loading.css',
-    target: '<link href="/css/loading.css" rel="stylesheet" type="text/css">',
-    end: (cssData) => `<style type="text/css">${cssData}</style>`
-  } ];
-
-  const cssInline = inlineOpts.map(opt => new Promise((resolve, reject) =>
-    fs.readFile(opt.html, 'utf8', (err, htmlData) =>
-      fs.readFile(opt.css, 'utf8', (err, cssData) => {
-        htmlData = htmlData.replace(opt.target, opt.end(cssData));
-        fs.outputFile(opt.html, htmlData, (err) => {
-          if (err) return reject(err);
-          resolve();
-        });
+  const cssInline = fs.readFile(targetHtml, 'utf8', (err, htmlData) =>
+    fs.readdirSync(`${cssDir}/inline`).map(cssFile =>
+      fs.readFile(`${cssDir}/inline/${cssFile}`, 'utf8', (err, cssData) => {
+        htmlData = htmlData.replace('<body>', `<style type="text/css">${cssData}</style>\n<body>`);
+        fs.outputFile(targetHtml, htmlData);
       })
-    )));
+    ));
 
-  await Promise.all(cssMin.concat(cssInline));
+  await Promise.all([ cssMin, cssInline ]);
 }
