ba52f8342f5aba4360223ea865f506441afc4a71
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-03-20 13:44:30 +0000

    Fonts for desktop too

--- tools/copy.js
+++ tools/copy.js
@@ -8,6 +8,8 @@ const fsPromise = Bluebird.promisifyAll(fs);
 export default async function copy() {
   const indexFile = argv.production ? 'index' : 'index-dev';
 
+  await fsPromise.copyAsync('./node_modules/kakapo-assets/icomoon/fonts', 'build/fonts', {});
+
   if (argv.platform === 'desktop') {
     proc.execSync('babel app/browser.js --out-file build/browser.js');
     await fsPromise.copyAsync('package.json', 'build/package.json', {});
@@ -19,7 +21,6 @@ export default async function copy() {
   }
 
   if (argv.platform === 'web') {
-    await fsPromise.copyAsync('./node_modules/kakapo-assets/icomoon/fonts', 'build/fonts', {});
     await fsPromise.copyAsync(`app/html/web/${indexFile}.html`, 'build/index.html', {});
     await fsPromise.copyAsync('app/html/web/app.html', 'build/app.html', {});
   }
