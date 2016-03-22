5a6ca437f6129f35a75b8dd59bfefcdc787d3228
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-03-22 21:17:21 +0000

    Fix for locale data in tests

--- test/test-bundler.js
+++ test/test-bundler.js
@@ -1,16 +1,14 @@
 require('es6-promise').polyfill();
 
 if (!global.Intl) {
-  global.Intl = require('intl'); // Intl polyfill
-  require('react-intl/locale-data/en');
+  global.Intl = require('intl');
+  require('intl/locale-data/jsonp/en');
 }
 
 localStorage.clear();
 
-// require all `test/components/**/index.js`
 const testsContext = require.context('./src', true, /-test\.(js|jsx)$/);
 testsContext.keys().forEach(testsContext);
 
-// require all `src/components/**/index.js`
 const componentsContext = require.context('../app/scripts/', true, /\*\.(js|jsx)$/);
 componentsContext.keys().forEach(componentsContext);
