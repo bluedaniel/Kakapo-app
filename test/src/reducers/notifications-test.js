a9b9ee93d6d3090567844ec8dbab9ff6bc03561c
Daniel Levitt <daniel.levitt@guardian.co.uk>
2016-03-23 08:55:03 +0000

    Better notify test

--- test/src/reducers/notifications-test.js
+++ test/src/reducers/notifications-test.js
@@ -1,16 +1,23 @@
 import { expect } from 'chai';
+import { store } from 'stores/configureStore';
 import { notifyActions } from 'actions/';
 import notifications, { initialState } from 'reducers/notifications';
 
 describe('Reducer `notifications`', () => {
   it('send a notification', () => {
-    const setup = notifications(initialState, notifyActions.notify('uniq1', 'test'));
-    expect(setup.count()).to.eql(1);
-    expect(setup.get('uniq1')).to.eql('test');
+    store.dispatch(notifyActions.send('test', 1000));
+    const state = store.getState();
+    expect(state.notifications.count()).to.eql(1);
+    expect(state.notifications.first()).to.eql('test');
 
-    it('clear a notification', () => {
-      const setup = notifications(initialState, notifyActions.clear('uniq1'));
-      expect(setup.count()).to.eql(0);
+    it('clear a notification after 1 second', (done) => {
+      const action = store.dispatch(notifyActions.send('test', 1000));
+      action.then(data => {
+        const setup = notifications(initialState, data);
+        expect(data.type).to.eql('NOTIFICATION_CLEAR');
+        expect(setup.count()).to.eql(0);
+        done();
+      });
     });
   });
 });
