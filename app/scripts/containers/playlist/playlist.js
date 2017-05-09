cf53e227046df81647e518507b263a96df770fa1
Daniel Levitt <daniellevitt86@gmail.com>
2017-05-09 22:47:09 +0100

    Updating more packages

--- app/scripts/containers/playlist/playlist.js
+++ app/scripts/containers/playlist/playlist.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Rx from 'rxjs';
 import Clipboard from 'clipboard';
 import shortid from 'shortid';
--- app/scripts/containers/settings/settings.js
+++ app/scripts/containers/settings/settings.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { ipcRenderer, remote } from 'electron';
 import { Link } from 'react-router';
 import { settingActions, themeActions } from 'actions/';
