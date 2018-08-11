20155a01655da807e94eab5e8e8e3114e0894631
Daniel Levitt <daniellevitt86@gmail.com>
2018-08-11 16:04:01 +0100

    Stub window.HTMLMediaElement

--- jest/setup.js
+++ jest/setup.js
@@ -2,23 +2,31 @@ import raf from 'raf';
 import fetchMock from 'jest-fetch-mock';
 import soundcloudMock from '../__mocks__/soundcloud.json';
 
+const noop = () => {};
+
 global.fetch = fetchMock;
 global.__WEB__ = true;
 global.__DESKTOP__ = false;
 
+// Mock for JSDOM not supporting HTMLMediaElement
+window.HTMLMediaElement.prototype.load = noop;
+window.HTMLMediaElement.prototype.play = noop;
+window.HTMLMediaElement.prototype.pause = noop;
+window.HTMLMediaElement.prototype.addTextTrack = noop;
+
 // AWS Custom build mock
-jest.mock('aws-custom-build', () => {});
+jest.mock('aws-custom-build', noop);
 
 // AWS DynamoDB mock
 function DynamoDB() {
-  return { getItem: () => {}, putItem: () => {} };
+  return { getItem: noop, putItem: noop };
 }
-global.AWS = { DynamoDB, config: { update: () => {} } };
+global.AWS = { DynamoDB, config: { update: noop } };
 
 // Soundcloud mock
 global.SC = {
-  initialize: () => {},
-  get: (name, obj, cb) => cb(soundcloudMock)
+  initialize: noop,
+  get: (name, obj, cb) => cb(soundcloudMock),
 };
 
 raf.polyfill(global);
