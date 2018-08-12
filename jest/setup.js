c92eed7316f32e19666b279bd91bbea05e0d227a
Daniel Levitt <daniellevitt86@gmail.com>
2018-08-12 11:25:49 +0100

    Jest mock must be inline func

--- jest/setup.js
+++ jest/setup.js
@@ -2,30 +2,28 @@ import raf from 'raf';
 import fetchMock from 'jest-fetch-mock';
 import soundcloudMock from '../__mocks__/soundcloud.json';
 
-const noop = () => {};
-
 global.fetch = fetchMock;
 global.__WEB__ = true;
 global.__DESKTOP__ = false;
 
 // Mock for JSDOM not supporting HTMLMediaElement
-window.HTMLMediaElement.prototype.load = noop;
-window.HTMLMediaElement.prototype.play = noop;
-window.HTMLMediaElement.prototype.pause = noop;
-window.HTMLMediaElement.prototype.addTextTrack = noop;
+window.HTMLMediaElement.prototype.load = () => {};
+window.HTMLMediaElement.prototype.play = () => {};
+window.HTMLMediaElement.prototype.pause = () => {};
+window.HTMLMediaElement.prototype.addTextTrack = () => {};
 
 // AWS Custom build mock
-jest.mock('aws-custom-build', noop);
+jest.mock('aws-custom-build', () => {});
 
 // AWS DynamoDB mock
 function DynamoDB() {
-  return { getItem: noop, putItem: noop };
+  return { getItem: () => {}, putItem: () => {} };
 }
-global.AWS = { DynamoDB, config: { update: noop } };
+global.AWS = { DynamoDB, config: { update: () => {} } };
 
 // Soundcloud mock
 global.SC = {
-  initialize: noop,
+  initialize: () => {},
   get: (name, obj, cb) => cb(soundcloudMock),
 };
 
